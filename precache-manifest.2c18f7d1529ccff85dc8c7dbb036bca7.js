5becba100a2b6a3da8253f4e72c3f348d3caf6f4
lcjf <jongo1@gmail.com>
2020-06-04 16:16:48 +0100

    first commit

---
+++ precache-manifest.2c18f7d1529ccff85dc8c7dbb036bca7.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae070820e731211575f18e6a0081e07f",
+    "url": "/categories/austria.json"
+  },
+  {
+    "revision": "ae070820e731211575f18e6a0081e07f",
+    "url": "/categories/nature.json"
+  },
+  {
+    "revision": "73c3a84c4ca2d525226b",
+    "url": "/css/app.0d51826d.css"
+  },
+  {
+    "revision": "6a68b9aa6424bad63144e1d696cf9e55",
+    "url": "/img/bg.6a68b9aa.png"
+  },
+  {
+    "revision": "1e53a8eebbc6830d39c76f4a253c63a1",
+    "url": "/img/motion-blur.1e53a8ee.jpg"
+  },
+  {
+    "revision": "6979fb4a1928773d1bb33508a7ce2d96",
+    "url": "/img/photography/animals/bears-playing.jpg"
+  },
+  {
+    "revision": "5308fdc74347cdef0aa93e256a10d4ee",
+    "url": "/img/photography/austria/gondola.jpg"
+  },
+  {
+    "revision": "1e53a8eebbc6830d39c76f4a253c63a1",
+    "url": "/img/photography/austria/motion-blur.jpg"
+  },
+  {
+    "revision": "c47fd3d88ac6a3ac8ba8086de6b60865",
+    "url": "/img/photography/nature/placeholder.jpg"
+  },
+  {
+    "revision": "80a64a1841ca1b36f589c6b02974d5aa",
+    "url": "/img/photography/nature/placeholder2.jpg"
+  },
+  {
+    "revision": "cb0c99f631faf2dd3159c7383d445885",
+    "url": "/img/photography/nature/placeholder3.jpg"
+  },
+  {
+    "revision": "67a6a0aa74d0b0f3c9c52cdb4043c0ee",
+    "url": "/img/photography/nature/placeholder4.jpg"
+  },
+  {
+    "revision": "4eda0684ae32e6a3fdb9b9a5c9b2dc08",
+    "url": "/img/photography/nature/placeholder5.jpg"
+  },
+  {
+    "revision": "c47fd3d88ac6a3ac8ba8086de6b60865",
+    "url": "/img/placeholder.c47fd3d8.jpg"
+  },
+  {
+    "revision": "80a64a1841ca1b36f589c6b02974d5aa",
+    "url": "/img/placeholder2.80a64a18.jpg"
+  },
+  {
+    "revision": "cb0c99f631faf2dd3159c7383d445885",
+    "url": "/img/placeholder3.cb0c99f6.jpg"
+  },
+  {
+    "revision": "67a6a0aa74d0b0f3c9c52cdb4043c0ee",
+    "url": "/img/placeholder4.67a6a0aa.jpg"
+  },
+  {
+    "revision": "4eda0684ae32e6a3fdb9b9a5c9b2dc08",
+    "url": "/img/placeholder5.4eda0684.jpg"
+  },
+  {
+    "revision": "30fb856f2c0eff01f9cfa6230dd5c3b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73c3a84c4ca2d525226b",
+    "url": "/js/app.032b2c96.js"
+  },
+  {
+    "revision": "c2e2f8b795217f04f438",
+    "url": "/js/chunk-vendors.051ed444.js"
+  },
+  {
+    "revision": "35d249fa68d18eb48d52d4bdce8b6138",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
